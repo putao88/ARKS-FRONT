3dc957f31a0a71c9914071cc7c6f9fba67a4a19a
Daisy <15773281560@163.com>
2022-11-20 21:39:10 +0800

    hide footer

--- src/views/components/core/View.vue
+++ src/views/components/core/View.vue
@@ -2,7 +2,7 @@
   <v-content class="content-wrap">
     <router-view />
 
-    <dashboard-core-footer />
+    <!-- <dashboard-core-footer /> -->
   </v-content>
 </template>
 
@@ -11,7 +11,7 @@
     name: 'DashboardCoreView',
 
     components: {
-      DashboardCoreFooter: () => import('./Footer'),
+      // DashboardCoreFooter: () => import('./Footer'),
     },
   }
 </script>
