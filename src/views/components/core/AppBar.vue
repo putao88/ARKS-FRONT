d327919224e24e070910437cb085ed16a5620b12
greatpie <greatpie@live.com>
2022-12-13 23:22:54 +0800

    fix: fetch back wallet code

--- src/views/components/core/AppBar.vue
+++ src/views/components/core/AppBar.vue
@@ -1,25 +1,9 @@
 <template>
-  <v-app-bar
-    id="app-bar"
-    fixed
-    app
-    flat
-    height="75"
-  >
-    <v-btn
-      class="mr-3"
-      elevation="1"
-      fab
-      small
-      @click="setDrawer(!drawer)"
-    >
-      <v-icon v-if="value">
-        mdi-view-quilt
-      </v-icon>
+  <v-app-bar id="app-bar" fixed app flat height="75">
+    <v-btn class="mr-3" elevation="1" fab small @click="setDrawer(!drawer)">
+      <v-icon v-if="value"> mdi-view-quilt </v-icon>
 
-      <v-icon v-else>
-        mdi-menu
-      </v-icon>
+      <v-icon v-else> mdi-menu </v-icon>
     </v-btn>
 
     <v-btn
@@ -50,9 +34,7 @@
       color="primary"
       @click="openClaimModal"
     >
-      <v-icon large>
-        mdi-bitcoin
-      </v-icon>
+      <v-icon large> mdi-bitcoin </v-icon>
       <span class="primary--text font-weight-bold">Claim</span>
     </v-btn>
 
@@ -66,13 +48,9 @@
       @click="openWeb3Modal"
     >
       <div class="wallet-icon-wrap">
-        <v-icon size="20">
-          mdi-wallet
-        </v-icon>
+        <v-icon size="20"> mdi-wallet </v-icon>
       </div>
-      <div class="primary--text font-weight-bold address-div">
-        Connect
-      </div>
+      <div class="primary--text font-weight-bold address-div">Connect</div>
     </v-btn>
 
     <v-menu
@@ -93,9 +71,7 @@
           v-on="on"
         >
           <div class="wallet-icon-wrap">
-            <v-icon size="20">
-              mdi-wallet
-            </v-icon>
+            <v-icon size="20"> mdi-wallet </v-icon>
           </div>
           <div class="primary--text font-weight-bold overflow-text address-div">
             {{ address }}
@@ -103,10 +79,7 @@
         </v-btn>
       </template>
 
-      <v-list
-        :tile="false"
-        nav
-      >
+      <v-list :tile="false" nav>
         <v-list-item
           v-for="(item, i) in subMenus"
           :key="i"
@@ -130,94 +103,97 @@
 
 <script>
 // Utilities
-  import { mapState, mapMutations } from 'vuex'
-  import ClaimModal from '@/components/ClaimModal'
+import { mapState, mapMutations } from 'vuex'
+import ClaimModal from '@/components/ClaimModal'
 
-  import { chain, configureChains, createClient, disconnect } from '@wagmi/core'
-  import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
-  import { Web3Modal } from '@web3modal/html'
+import { chain, configureChains, createClient, disconnect,watchAccount } from '@wagmi/core'
+import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/html'
 
-  // 1. Define constants
-  const projectId = 'ef558420ed3676cd88ef972a3b072453'
-  const chains = [chain.polygon]
+// 1. Define constants
+const projectId = 'ef558420ed3676cd88ef972a3b072453'
+const chains = [chain.polygon]
 
-  // 2. Configure wagmi client
-  const { provider } = configureChains(chains, [walletConnectProvider({ projectId })])
-  const wagmiClient = createClient({
-    autoConnect: true,
-    connectors: modalConnectors({ appName: 'web3Modal', chains }),
-    provider,
-  })
+// 2. Configure wagmi client
+const { provider } = configureChains(chains, [walletConnectProvider({ projectId })])
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: 'web3Modal', chains }),
+  provider,
+})
 
-  // 3. Create ethereum and modal clients
-  const ethereumClient = new EthereumClient(wagmiClient, chains)
-  export const web3Modal = new Web3Modal({ projectId }, ethereumClient)
+// 3. Create ethereum and modal clients
+const ethereumClient = new EthereumClient(wagmiClient, chains)
+export const web3Modal = new Web3Modal({ projectId }, ethereumClient)
 
-  export default {
-    name: 'DashboardCoreAppBar',
-    components: {
-      ClaimModal,
+export default {
+  name: 'DashboardCoreAppBar',
+  components: {
+    ClaimModal,
+  },
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
     },
-    props: {
-      value: {
-        type: Boolean,
-        default: false,
+  },
+
+  data: () => ({
+    subMenus: [
+      {
+        icon: 'mdi-store',
+        text: 'Deposite',
       },
-    },
+      {
+        icon: 'mdi-logout',
+        text: 'Disconnect',
+      },
+    ],
+    showClaimModal: false,
+  }),
 
-    data: () => ({
-      subMenus: [
-        {
-          icon: 'mdi-store',
-          text: 'Deposite',
-        },
-        {
-          icon: 'mdi-logout',
-          text: 'Disconnect',
-        },
-      ],
-      showClaimModal: false,
+  computed: {
+    ...mapState(['drawer', 'connected', 'address']),
+  },
+  mounted: function () {
+    watchAccount(account => {
+      if (account.isConnected) {
+        this.setConnected(true)
+        this.setAddress(account.address)
+      }else{
+        this.setConnected(false)
+        this.setAddress('')
+      }
+    })
+  },
+  methods: {
+    ...mapMutations({
+      setDrawer: 'SET_DRAWER',
+      setConnected: 'SET_CONNECTED',
+      setAddress: 'SET_ADDRESS',
     }),
-
-    computed: {
-      ...mapState(['drawer', 'connected', 'address']),
+    goBack() {
+      this.$router.go(-1)
     },
-    mounted: function () {
-      // const unwatch = watchAccount(account => {
-      //   if (account.isConnected) {
-      //     this.setConnected(true)
-      //     this.setAddress(account.address)
-      //   }
-      // })
+    menuHandle(method) {
+      if (method === 'Disconnect') {
+        disconnect()
+        this.setConnected(false)
+      } else {
+        this.$router.push('/launchpad')
+      }
     },
-    methods: {
-      ...mapMutations({
-        setDrawer: 'SET_DRAWER',
-        setConnected: 'SET_CONNECTED',
-        setAddress: 'SET_ADDRESS',
-      }),
-      goBack () {
-        this.$router.go(-1)
-      },
-      menuHandle (method) {
-        if (method === 'Disconnect') {
-          disconnect()
-          this.setConnected(false)
-        } else {
-          this.$router.push('/launchpad')
-        }
-      },
-      openClaimModal () {
-        this.showClaimModal = true
-      },
-      closeClaimModal () {
-        this.showClaimModal = false
-      },
-      openWeb3Modal () {
-        web3Modal.openModal()
-      },
+    openClaimModal() {
+      this.showClaimModal = true
     },
-  }
+    closeClaimModal() {
+      this.showClaimModal = false
+    },
+    openWeb3Modal() {
+      web3Modal.openModal()
+    },
+  },
+}
 
 </script>
 <style lang="scss" scoped>
